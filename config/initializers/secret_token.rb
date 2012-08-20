30582a5ca428e620f3bbc4095a9c97d90f787e8f
Javier C <youracow@gmail.com>
2012-08-19 20:25:07 -0600

    beginning of a new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitulous::Application.config.secret_token = '38284080649a2fe98bd4bed940ba21d77dab1f2859b9c9b5294aa148c33be42cd2ce8987c2062afc23ba33db542d4dae59f56fb5bcd91e6e228da4147d2a8ff9'
